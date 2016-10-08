d8605840dbcf6c22627782c07a14e1f7ad60b4fb
nixawk <hap.ddup@gmail.com>
2016-10-08 07:41:52 -0500

    stdio.h

---
+++ hello-popen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+$ echo id | hello-open
+*/
+
+int main(int argc, const char *argv[])
+{
+	FILE *in;
+	const int buffsize = 10240;
+	char buff[buffsize];
+	char command[buffsize];
+
+	scanf("%s", command);
+
+	if (!(in = popen(command, "r")))
+	{
+	    return 1;
+	}
+
+	while(fgets(buff, sizeof(buff), in) != NULL)
+	{
+			printf("%s", buff);
+	}
+
+	pclose(in);
+
+	return 0;
+}
