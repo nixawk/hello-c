91c2548a4e33666f1a947833688732d6453e84be
nixawk <hap.ddup@gmail.com>
2017-08-28 05:17:13 -0400

    int main() VS int main(void)

---
+++ hello_main_void.c
@@ -0,0 +1,26 @@
+/*
+
+// http://www.geeksforgeeks.org/difference-int-main-int-mainvoid/
+
+#include <stdio.h>
+int main()            // int main() can be called with any number of arguments, 
+{
+    static int i = 5;
+    if (--i){
+        printf("%d ", i);
+        main(10);    // It will be compiled, and executed.
+    }
+}
+
+*/
+
+
+#include <stdio.h>
+int main(void)       // int main(void) can only be called without any argument.
+{
+    static int i = 5;
+    if (--i){
+        printf("%d ", i);
+        main(10);    // compilation error.
+    }
+}
