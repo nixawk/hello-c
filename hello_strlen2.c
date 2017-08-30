f604940d8b7a3858d8bcbd613ba60d361e896576
nixawk <hap.ddup@gmail.com>
2017-08-30 05:27:29 -0400

    strlen from apple - opensource

---
+++ hello_strlen2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+size_t
+_strlen(const char *str)
+{
+    const char *s;
+
+    for (s = str; *s; s++);
+    return (s - str);
+}
+
+
+int
+main(void)
+{
+    char s[] = "helloworld";
+
+    printf("%d\n", _strlen(s));
+    return 0;
+}
+
+
+// https://opensource.apple.com/source/Libc/Libc-167/gen.subproj/i386.subproj/strlen.c
