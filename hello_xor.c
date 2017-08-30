965a4af747d41ccbd6cb01df427a3f97390fb862
nixawk <hap.ddup@gmail.com>
2017-08-30 05:34:47 -0400

    csapp.3e - inplace_swap

---
+++ hello_xor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void
+inplace_swap(int *x, int *y)
+{
+    *x ^= *y;
+    *y ^= *x;
+    *x ^= *y;
+}
+
+
+int
+main(void)
+{
+    int x = 1, y = 2;
+
+    printf("x = %d, y = %d\n", x, y);
+    inplace_swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+
+
+// Computer Systems: A Programmer's Perspective, 3/E (CS:APP3e). Part 2.1.7
