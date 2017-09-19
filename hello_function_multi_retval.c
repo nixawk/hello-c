3f696d2f2ada2d7bf04e99eacb57aaae59b447cc
nixawk <hap.ddup@gmail.com>
2017-09-19 00:20:35 -0400

    function - return multi vals

---
+++ hello_function_multi_retval.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* A function in C can return only one value.
+   How to return multiple values from a function ? */
+struct newtype {
+    int x;
+    int y;
+};
+
+
+/* return multiple values from a function */
+struct newtype
+demo(int x, int y)
+{
+    struct newtype st = {x, y};
+    return st;
+}
+
+
+int
+main(void)
+{
+    struct newtype st = demo(1, 2);
+    printf("x = %d, y = %d\n", st.x, st.y);
+    return 0;
+}
+
+/* If we want the function to return multiple values of same data types,
+  the pointer can be a good choice. */
+
+
+// http://www.geeksforgeeks.org/how-can-i-return-multiple-values-from-a-function/
