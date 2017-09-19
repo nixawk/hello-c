1d2c36425c73c1f2e37d572759d428d11a627248
nixawk <hap.ddup@gmail.com>
2017-09-19 01:50:52 -0400

    add abort desc

--- hello_abort.c
+++ hello_abort.c
@@ -9,20 +9,26 @@ abort the program execution and comes out directly from the place of the call.
 
 This function does not return any value.
 
+Unlike exit() function, abort() may not close files that are open.
+It may also not delete temporary files and may not flush stream buffer.
+Also, it does not call functions registered with atexit().
+
+This function actually terminates the process by raising a SIGABRT signal,
+
 */
 
 int main(int argc, char const *argv[]) {
-  FILE *fp;
+    FILE *fp;
 
-  printf("Going to open nofile.txt\n");
-  fp = fopen("nofile.txt", "r");
-  if (fp == NULL)
-  {
-    printf("Going to abort the program\n");
-    abort();
-  }
-  printf("Going to close nofile.txt\n");
-  fclose(fp);
+    fp = fopen("nofile.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Going to abort the program\n");
+        abort();
+    }
 
-  return 0;
+    printf("Going to close nofile.txt\n");
+    fclose(fp);
+
+    return 0;
 }
