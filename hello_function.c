587a3d86f853b6b67a8fdd5279c08aa57b752cb1
nixawk <hap.ddup@gmail.com>
2017-09-18 21:11:27 -0400

    function - pass_by_value / pass_by_reference

--- hello_function.c
+++ hello_function.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
 
+/*
+    Function declaration tells compiler about number of parameters functions takes, date-types
+    of parameters and return type of function.
+*/
 
-int add_demo(int x, int y)
+/*
+    It is always recommended to declare a function before it is used.
+*/
+int add_by_value(int, int);
+int add_by_refer(int *, int *);
+
+/*
+    Pass by Value:
+        In this parameter passing method, values of actual parameters are copied to function's
+        formal parameter and two types of parameters are stored in different memory locations.
+        So any changes made inside functions are not reflected in actual parameters of caller.
+
+    Pass by Reference:
+        Both actual and formal parameters refer to same locations, so any changes made inside
+        the function are actually reflected in actual parameters of caller.
+*/
+int add_by_value(int x, int y)
 {
     return x + y;
 }
 
+int add_by_refer(int *x, int *y)
+{
+    *x = 11;
+    *y = 22;
+    return *x + *y;
+}
+
 int main(void)
 {
-    int x, y, z;
+    /*
+        The parameters passed to function are called (actual parameters). Ex: 10, 20
+        The parameters received by function are called formal parameters.
+    */
 
-    printf("Please enter a number - x: ");
-    scanf("%d", &x);
+    int x = 10, y = 20, z;
 
-    printf("Please enter a number - y: ");
-    scanf("%d", &y);
+    z = add_by_value(x, y);
+    printf("%d + %d = %d\n", x, y, z);
 
-    z = add_demo(x, y);
-    printf("x + y = %d\n", z);
+    z = add_by_refer(&x, &y);
+    printf("%d + %d = %d\n", x, y, z);
 
     return 0;
 }
