6a47e23d878947d0f9fa774b680c3b3083848e35
nixawk <hap.ddup@gmail.com>
2017-09-22 10:49:39 -0400

    add errno desc

--- hello_errno.c
+++ hello_errno.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
+
+// Error Reporting
+
+// Many functions in the GNU C Library detect and report error conditions,
+// and sometimes your programs need to check for these error conditions.
+
+
+// Checking for Errors
+
+// Many library functions return a sepcial value to indicate that they have failed.
+// To find out what kind of error it was, you need to look at the error code stored
+// in the variable errno.
+
+// This variable is declared in the header file [errno.h]
+
+/* volatile int errno */
+
+
+// Error Codes
+
+
+// Error Message
+// char * strerror (int errnum)
+// char * strerror_r (int errnum, char *buf, size_t n)
+
 
 int main(void)
 {
-    fprintf(stderr, "Value of errno: %d\n", 1024);
+    // char *err = strerror(1);
+    fprintf(stderr, "Value of errno: %d\n", EPERM);
+    fprintf(stderr, "Error msg: %s", strerror(1));
     return 0;
 }
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Error-Reporting.html#Error-Reporting
+// https://www.gnu.org/software/libc/manual/html_node/Checking-for-Errors.html#Checking-for-Errors
+// https://www.gnu.org/software/libc/manual/html_node/Error-Codes.html#Error-Codes
+// https://www.gnu.org/software/libc/manual/html_node/Error-Messages.html#Error-Messages
