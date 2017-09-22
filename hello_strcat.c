20cbcbf8c44ee8b32ffe62d90d2e984708835cad
nixawk <hap.ddup@gmail.com>
2017-09-21 22:52:06 -0400

    add strcat desc

--- hello_strcat.c
+++ hello_strcat.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
-    char src[] = "helloworld";
-    char dest[256];
-    char *strcat_pointer;
 
-    memset(dest, '\x00', 256);
-    strcat_pointer = strcat(dest, src);
-    printf("destination string: %s\n", dest);
-    printf("destination string: %s\n", strcat_pointer);
+// char * strcat (char *restrict to, const char *restrict from)
+// wchar_t * wcscat (wchar_t *restrict wto, const wchar_t *restrict wfrom)
+
+
+// The strcat function is similar to strcpy, except that the bytes [from]
+// are concatenated or appended to the end of [to], instead of overwriting it.
+
+
+// It is almost always unnecessary to use strcat.
+
+
+#define SIZE 256
+
+
+void
+strcat_usage()
+{
+    char dst[SIZE] = "hello, ";
+    char src[SIZE] = "world";
+
+    printf("before strcat: %s\n", dst);
+    strcat(dst, src);
+    printf("after  strcat: %s\n", dst);
+}
+
+
+int
+main(void)
+{
+    strcat_usage();
     return 0;
 }
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Concatenating-Strings.html#Concatenating-Strings
