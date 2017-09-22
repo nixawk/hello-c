533125bac40882f9e9eecd6cdb383c7017f6beac
nixawk <hap.ddup@gmail.com>
2017-09-21 23:18:32 -0400

    add strcmp desc

--- hello_strcmp.c
+++ hello_strcmp.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(void)
+
+// int memcmp (const void *a1, const void *a2, size_t size)
+// int wmemcmp (const wchar_t *a1, const wchar_t *a2, size_t size)
+// int strcmp (const char *s1, const char *s2)
+// int wcscmp (const wchar_t *ws1, const wchar_t *ws2)
+// int strcasecmp (const char *s1, const char *s2)
+// int wcscasecmp (const wchar_t *ws1, const wchar_t *ws2)
+// int strncmp (const char *s1, const char *s2, size_t size)
+// int wcsncmp (const wchar_t *ws1, const wchar_t *ws2, size_t size)
+// int strncasecmp (const char *s1, const char *s2, size_t n)
+// int wcsncasecmp (const wchar_t *ws1, const wchar_t *s2, size_t n)
+// int strverscmp (const char *s1, const char *s2)
+// int bcmp (const void *a1, const void *a2, size_t size)
+
+
+void
+strcmp_usage(char *inputstr)
 {
-    int ret;
+    char flag[] = "hello";
+    int ret = strcmp(flag, inputstr);
+    // If the two strings are equal, strcmp returns 0.
 
-    char astring1[] = "Helloworld1";
-    char astring2[] = "Helloworld1";
+    printf("%d\n", ret);  /* ret = s1 - str */
+}
 
-    ret = strcmp(astring1, astring2); /* 0 - equal, -1 - not equal */
 
-    if (0 == ret) {
-        printf("astring1 is equal to astring2\n");
+int
+main(int argc, const char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("[*] %s <hello>", argv[0]);
+        exit(0);
     }
-    else {
-        printf("astring1 is not equal to astring2\n");
-    }
+
+    strcmp_usage((char *)argv[1]);
 
     return 0;
 }
+
+
+// https://www.gnu.org/software/libc/manual/html_node/String_002fArray-Comparison.html#String_002fArray-Comparison
