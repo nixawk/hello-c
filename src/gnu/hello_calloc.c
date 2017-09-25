75ad64feb3b15dc05686655d5ca577acb58cc528
nixawk <hap.ddup@gmail.com>
2017-09-24 20:57:08 -0400

    add calloc desc

--- src/gnu/hello_calloc.c
+++ src/gnu/hello_calloc.c
@@ -2,40 +2,31 @@
 #include <stdio.h>
 
 
-/*
+// void * calloc (size_t count, size_t eltsize)
 
-void *calloc(size_t, size_t size);
+// The function [calloc] allocates memory and clears it to zero. Iti is declared in [stdlib.h]
 
-allocates the requested memory and returns a pointer to it. The difference in
-malloc and calloc is that malloc does not set the memory to zero where as calloc
-sets allocated memory to zero.
+void
+calloc_usage(void)
+{
+    int *ptr = calloc(1, sizeof(int));
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "calloc() failed\n");
+        exit(0);
+    }
 
-- nitems - This is number of elements to be allocated.
-- size - This is the size of elements.
-
-This function returns a pointer to the allocated memory, or NULL if the request
-fails.
-
-*/
-
-int main(int argc, char const *argv[]) {
-  int i, n;
-  int *pointer;
-
-  printf("Number of elements to be entered: ");
-  scanf("%d", &n);
-
-  pointer = (int *)calloc(n, sizeof(int));
-  printf("Enter %d number: \n", n);
-  for (i = 0; i < n; ++i)
-  {
-      scanf("%d", &pointer[i]);
-  }
-
-  printf("The numbers entered are: ");
-  for (i = 0; i < n; i++)
-  {
-      printf("%d ", pointer[i]);
-  }
-  return 0;
+    printf("%d\n", *ptr);  // print 0
+    free(ptr);             // cleanup
 }
+
+
+int
+main(void)
+{
+    calloc_usage();
+    return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Allocating-Cleared-Space.html#Allocating-Cleared-Space
