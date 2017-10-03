e3dfe3f7321d6cf4578183d56f240ff59ee1a7f6
nixawk <hap.ddup@gmail.com>
2017-10-03 01:51:51 -0400

    add setenv() / putenv()

---
+++ src/gnu/Program Basics/hello_putenv.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//  int putenv (char *string)
+
+// The [putenv] function adds or removes definitions from the environment.
+// If the string is of the form 'name=value', the definition is added to
+// the environment. Otherwise, the string is interpreted as the name of
+// an environment variable, and any definition for this variable in the
+// environment is removed.
+
+// If the function is successful it returns 0. Otherwise the return value is
+// nonzero and [errno] is set to indicate the error.
+
+void
+putenv_usage(void)
+{
+    if (0 == putenv("LL='ls -l'"))
+    {
+        printf("LL=%s\n", getenv("LL"));
+    }
+}
+
+
+int
+main(void)
+{
+    putenv_usage();
+    return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Environment-Access.html#Environment-Access
---
+++ src/gnu/Program Basics/hello_setenv.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+    // int setenv (const char *name, const char *value, int replace)
+
+// The [setenv] function can be used to add a new definition to the environment.
+// The entry with the name [name] is replaced by the value 'name=value'.
+// Please note that this is also true if value is the value is the empty string.
+
+// To do this a new string is created and the strings [name] and [value] are copied.
+// A null pointer for the [value] parameter is illegal. If the environment already
+// contains an entry with key [name] the replace parameter controls the actions.
+
+// If replace is 0, nothing happens. Otherwise the old entry is replaced by the new one.
+
+// If the function is successful it returns 0. Otherwise the environment is unchanged
+// and the return value is -1 and errno is set.
+
+
+    // int unsetenv (const char *name)
+
+// The function returns -1 if name is a null pointer, points to an empty string, 
+// or points to a string containing a = character. It returns 0 if the call succeeded.
+
+
+    // int clearenv (void)
+
+// The [clearenv] function removes all entries from the environment.
+// Using [putenv] and [setenv] new entries can be added again later.
+// If the function is successful it returns 0. Otherwise the return value is nonzero.
+
+
+void
+setenv_usage(void)
+{
+    if (0 == setenv("LL", "'ls -l'", 0))
+    {
+        printf("LL=%s\n", getenv("LL"));  // output: LL='ls -l'
+        unsetenv("LL");
+        printf("LL=%s\n", getenv("LL"));  // output: LL=(null)
+    }
+}
+
+
+int
+main(void)
+{
+    setenv_usage();
+    return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Environment-Access.html#Environment-Access
