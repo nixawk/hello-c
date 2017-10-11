65b8b63676d93e049ec4d841b1f0e7a116959b4c
nixawk <hap.ddup@gmail.com>
2017-10-11 19:59:30 -0400

    add atexit() desc

--- src/gnu/Program_Termination/hello_atexit.c
+++ src/gnu/Program_Termination/hello_atexit.c
@@ -1,28 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// int atexit (void (*function) (void))
+
+// The [atexit] function registers the function [function] to be called at
+// normal program termination. The [function] is called with no arguments.
+
+// The return value from [atexit] is zero on success and nonzero if the function
+// cannot be registered.
+
+
+void
+atexit_callback(void)
+{
+    printf("call atexit callback function\n");
+}
+
+
+void
+atexit_usage(void)
+{
+    if (atexit(atexit_callback) != 0)
+    {
+        fprintf(stderr, "atexit() failed\n");
+    } else {
+        printf("register atexit callback function\n");
+    }
+}
+
+
+int main(int argc, char const *argv[]) {
+    atexit_usage();
+    printf("main function\n");
+
+    return 0;
+}
+
 /*
 
-int atexit(void (*func)(void));
-
-- func -- This is the function to be called at the termination of the program.
-
-This function returns a zero value if the function is registered successfully,
-otherwise a non-zero value is returned if it is failed.
+$ ./a.out
+register atexit callback function
+main function
+call atexit callback function
 
 */
 
-void callback(void)
-{
-    printf("callback function\n");
-}
-
-int main(int argc, char const *argv[]) {
-  /* register the termination function */
-  atexit(callback);
-
-  printf("Starting main program...\n");
-  printf("Exiting  main program...\n");
-
-  return 0;
-}
+// https://www.gnu.org/software/libc/manual/html_node/Cleanups-on-Exit.html
