d05d3daba05355794212eebca785c352a5cfa60b
nixawk <hap.ddup@gmail.com>
2017-10-11 20:12:53 -0400

    desc main() (char *envp[]) argument

---
+++ src/gnu/The_Basic_Program-System_Interface/hello_main_envp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// The argument [envp] gives the program's environment; it is the same as
+// the value of [environ].
+
+int
+main(int argc, const char *argv[], const char *envp[])
+{
+    int i;
+
+    for (i = 0; envp[i] != NULL; i++)
+    {
+        printf("%d: %s\n", i, envp[i]);
+    }
+
+    return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Program-Arguments.html
