4834d968dea80760f156e4a4685a29ccf55f2c7f
nixawk <hap.ddup@gmail.com>
2017-10-13 12:17:04 -0400

    add warn()

---
+++ src/gnu/02.Error Reporting/hello_warn.c
@@ -0,0 +1,33 @@
+#include <err.h>
+
+    // void warn (const char *format, …)
+
+// The warn function is roughly equivalent to a call like
+
+    //  error (0, errno, format, the parameters)
+
+// except that the global variables error respects and modifies are not used.
+
+
+void
+warn_usage(void)
+{
+    warn("this is a warn message");
+    warn("this is another warn message");
+}
+
+
+int
+main(void)
+{
+    warn_usage();
+    return 0;
+}
+
+/*
+$ ./a.out
+a.out: this is a warn message: Undefined error: 0
+a.out: this is another warn message: Undefined error: 0
+*/
+
+// https://www.gnu.org/software/libc/manual/html_node/Error-Messages.html#Error-Messages
