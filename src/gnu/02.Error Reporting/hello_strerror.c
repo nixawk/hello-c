46bcf0642f91445676e1576f281cbdcedf6caaa4
nixawk <hap.ddup@gmail.com>
2017-10-13 05:35:33 -0400

    add strerror() desc

--- src/gnu/02.Error Reporting/hello_strerror.c
+++ src/gnu/02.Error Reporting/hello_strerror.c
@@ -1,26 +1,43 @@
+#define _GNU_SOURCE
+#include <errno.h>
 #include <string.h>
 #include <stdio.h>
-#include <errno.h>
 
-/*
+// The library has functions and variables designed to make it easy for your program
+// to report information error messages in the customary format about the failure of
+// a library call. The functions [strerror] and [perror] give you the standard error
+// message for a given error code; the variable [program_invocation_short_name] gives
+// you convenient access to the name of the program that encountered the error.
 
-char *strerror(int errnum)
+    // char * strerror (int errnum)
 
-errnum - This is the error number, usually errno.
+// The [strerror] function maps the error code specified by the [errnum] argument
+// to a descriptive error message string. The return value is a pointer to this
+// string.
 
-This function returns a pointer to the error string describing error errnum.
+// The value [errnum] normally comes from the variable [errno].
+// You should not modify the string returned by [strerror]. Also, if you make subsequent
+// calls to [strerror], the string might be overwritten.
 
-*/
+// The function [strerror] is declared in [string.h]
+
+
+void
+strerror_usage(void)
+{
+    error = 1;  // raise an error status
+
+    printf("program name: %s\n", program_invocation_short_name);  // #define _GNU_SOURCE
+    printf("program err : %s\n", strerror(errno));                // errno is global.
+}
+
 
 int main(void)
 {
-    FILE *fp;
-
-    fp = fopen("file.txt", "r");
-    if (fp == NULL)
-    {
-        printf("Error: %s\n", strerror(errno));
-    }
-
+    strerror_usage();
     return 0;
 }
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Error-Messages.html#Error-Messages
+// https://linux.die.net/man/3/program_invocation_short_name
