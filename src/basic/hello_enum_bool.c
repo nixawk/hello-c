fa24f0b8b8502a7584fa933b7d067bcb018791ac
nixawk <hap.ddup@gmail.com>
2017-10-16 05:46:05 -0400

    enum -> bool type

---
+++ src/basic/hello_enum_bool.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// When you want to define names with constant integer values,
+// use enum rather than ‘#define’. 
+
+typedef enum {
+    false,
+    true
+} bool;
+
+
+int
+main(void)
+{
+    bool ret = true;
+
+    if (ret)
+    {
+        printf("condition is true;\n");
+    }
+
+    return 0;
+}
+
+
+// https://www.gnu.org/prep/standards/standards.html#Names
