51b46e33e6c2f095f2334e88cf1bc0058938d262
nixawk <hap.ddup@gmail.com>
2017-10-16 23:15:25 -0400

    Library Interpositioning - COMPILETIME

---
+++ src/gnu/01.Introduction/library_interpositioning/COMPILETIME/mymalloc.c
@@ -0,0 +1,24 @@
+#ifdef COMPILETIME
+
+#include <stdio.h>
+#include <malloc.h>
+
+
+/* malloc wrapper function */
+void*
+mymalloc(size_t size)
+{
+    void *ptr = malloc(size);
+    printf("malloc(%zu)=%p\n", size, ptr);
+    return ptr;
+}
+
+/* free wrapper function */
+void
+myfree(void *ptr)
+{
+    free(ptr);
+    printf("free(%p)\n", ptr);
+}
+
+#endif  // library interpositioning
