fa9897d0203c57e27d04b3f87b897e4ecdf9c4bc
nixawk <hap.ddup@gmail.com>
2017-10-16 23:15:56 -0400

    Library interpositioning - LINKTIME

---
+++ src/gnu/01.Introduction/library_interpositioning/LINKTIME/mymalloc.c
@@ -0,0 +1,23 @@
+#ifdef LINKTIME
+#include <stdio.h>
+
+void *__real_malloc(size_t size);
+void __real_free(void *ptr);
+
+/* malloc wrapper function */
+void *__wrap_malloc(size_t size)
+{
+    void *ptr = __real_malloc(size);  /* Call libc malloc */
+    printf("malloc(%zu) = %p\n", size, ptr);
+    return ptr;
+}
+
+
+/* free wrapper function */
+void __wrap_free(void *ptr)
+{
+    __real_free(ptr);
+    printf("free(%p)\n", ptr);
+}
+
+#endif
