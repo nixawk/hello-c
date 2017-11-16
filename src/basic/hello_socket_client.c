63f442030897bdbf96d2e417cbd40716b560e3d9
nixawk <hap.ddup@gmail.com>
2017-11-15 20:16:48 -0500

    socket client/server

---
+++ src/basic/hello_socket_client.c
@@ -0,0 +1,64 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+#define HOST "127.0.0.1"
+#define PORT 4444
+#define SIZE 1024
+
+
+void
+socket_client(void)
+{
+    struct sockaddr_in address, serv_addr;
+    int client_fd = 0, valread;
+    char hello[] = "hello from client";
+    char buffer[SIZE] = {0};
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port   = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary from
+    if (inet_pton(AF_INET, HOST, &serv_addr.sin_addr) <= 0)
+    {
+        perror("inet_pton failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))
+    {
+        perror("connect failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connection to %s %d port [tcp/*] succeeded!\n", HOST, PORT);
+    send(client_fd, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    valread = read(client_fd, buffer, 1024);
+    printf("%s\n", buffer);
+
+    close(client_fd);
+}
+
+
+int
+main(int argc, const char *argv[])
+{
+    socket_client();
+    return 0;
+}
+
+// http://www.geeksforgeeks.org/socket-programming-cc/
---
+++ src/basic/hello_socket_server.c
@@ -0,0 +1,81 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define PORT 4444
+#define SIZE 1024
+
+
+void
+socket_server(void)
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address; 
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[SIZE] = {0};
+    char hello[] = "hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Listening on [0.0.0.0] (family 0, port %d)\n", PORT);
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    valread = read(new_socket, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    close(new_socket);
+    close(server_fd);
+}
+
+
+int
+main(int argc, const char *argv[])
+{
+    socket_server();
+    return 0;
+}
+
+
+// http://www.geeksforgeeks.org/socket-programming-cc/
