239f1cd7440792de73ba6b1f14a03ae05421f589
nixawk <hap.ddup@gmail.com>
2017-12-06 03:13:19 -0500

    add program_invocation_name / program_invocation_short_name

---
+++ src/gnu/02.Error Reporting/hello_program_invocation_name.c
@@ -0,0 +1,43 @@
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <stdio.h>
+
+// char * program_invocation_name
+
+// The variable's value is the name that was used to invoke the program
+// running in the current process. It is the same as argv[0].
+// Note that this is not necessarily a useful file name; often it contains
+// no directory names.
+
+// The variable is a GNU extension and is declared in errno.h
+
+/*
+$ gcc hello_program_invocation_name.c -o hello_program_invocation_name
+$ ~/src/glibc/hello_program_invocation_name
+/home/code/src/glibc/hello_program_invocation_name
+
+*/
+
+void
+show_program_invocation_name(void)
+{
+    printf("%s\n", program_invocation_name);
+}
+
+
+int
+main(void)
+{
+    show_program_invocation_name();
+    return 0;
+}
+
+/*
+
+error: ‘program_invocation_name’ undeclared (first use in this function)
+
+#define _GNU_SOURCE
+
+*/
+// https://www.gnu.org/software/libc/manual/html_mono/libc.html#Error-Reporting
---
+++ src/gnu/02.Error Reporting/hello_program_invocation_short_name.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <stdio.h>
+
+
+// char * program_invocation_short_name
+
+// The variable is the name that was used to invoke the program running
+// in the currnt process, with directory names removed.(That is to say,
+// it is the same as program_invocation_name minus everything up to the
+// last slash, if any.)
+
+// The variable is GNU extension and is declared in errno.h
+
+/*
+
+$ gcc hello_program_invocation_short_name.c -o hello_program_invocation_short_name
+$ ~/src/glibc/hello_program_invocation_short_name 
+hello_program_invocation_short_name
+*/
+
+void
+show_program_invocation_short_name(void)
+{
+    printf("%s\n", program_invocation_short_name);
+}
+
+
+int
+main(void)
+{
+    show_program_invocation_short_name();
+    return 0;
+}
+
+
+/*
+
+error: ‘program_invocation_name’ undeclared (first use in this function)
+
+#define _GNU_SOURCE
+
+*/
+// https://www.gnu.org/software/libc/manual/html_mono/libc.html#Error-Reporting
