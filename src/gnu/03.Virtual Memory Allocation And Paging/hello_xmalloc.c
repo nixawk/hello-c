4fddbe6566543497f0c7ad1e4e8ce77c599a66a3
nixawk <hap.ddup@gmail.com>
2017-12-06 23:15:21 -0500

    add xmalloc

---
+++ src/gnu/03.Virtual Memory Allocation And Paging/hello_xmalloc.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
+// If no more space is available, malloc returns a null pointer. You should
+// check the value of every call to malloc. It is useful to write a subroutine
+// that calls malloc and reports an error if the value is a null pointer,
+// returning only if the value is nonzero. This function is conventionally
+// called xmalloc. Here it is:
+
+static void fatal(char *fmt, ...)
+{
+    va_list list;
+
+    va_start(list, fmt);
+    vfprintf(stderr, fmt, list);
+    va_end(list);
+
+    exit(1);
+}
+
+
+void *
+xmalloc(size_t size)
+{
+    void *value = malloc(size);
+    if (value == 0)
+        fatal("virtual memory exhausted");
+    return value;
+}
+
+
+int
+main(void)
+{
+    char *ptr = xmalloc(32);
+    free(ptr);
+}
+
+
+
+// https://www.gnu.org/software/libc/manual/html_mono/libc.html#Malloc-Examples
+// https://elixir.free-electrons.com/linux/latest/source/arch/um/os-Linux/start_up.c#L73
