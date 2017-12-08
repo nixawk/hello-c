9b3e031a0f3f031c21652026b3e83634983fbd67
nixawk <hap.ddup@gmail.com>
2017-12-08 09:37:58 -0500

    Fix compile command

--- src/basic/hello_libcurl/cacertinmem.c
+++ src/basic/hello_libcurl/cacertinmem.c
@@ -29,7 +29,7 @@
 #include <stdio.h>
 
 
-// gcc -lcurl -I /usr/local/opt/openssl/include -L /usr/local/opt/openssl/lib cacertinmem.c
+// gcc cacertinmem.c -L/usr/lib -lssl -lcrypto -lcurl
 
 
 size_t
