1f7c5138e19faaea2e47e2afd59d9271540ae154
nixawk <hap.ddup@gmail.com>
2017-12-11 01:44:27 -0500

    add wmemcpy

---
+++ src/gnu/05.String and Array Utilities/hello_wmemcpy.c
@@ -0,0 +1,34 @@
+#include <wchar.h>
+#include <stdio.h>
+
+
+    // wchar_t * wmemcpy (wchar_t *restrict wto, const wchar_t *restrict wfrom, size_t size)
+
+// The wmemcpy function copies size wide characters from the object beginning
+// at wfrom into the object beginning at wtp. The behavior of this function
+// is underfined if the two arrays wto and wfrom overlap; use wmemmove instead
+// if overlapping is possible.
+
+#define BUFSIZE 512
+
+
+void
+wmemcpy_usage(void)
+{
+    wchar_t src[BUFSIZE] = L"hello, world";
+    wchar_t dst[BUFSIZE] = {};
+
+    wmemcpy(dst, src, wcslen(src));
+    wprintf(dst);
+}
+
+
+int
+main(void)
+{
+    wmemcpy_usage();
+    return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_mono/libc.html#Character-Handling
