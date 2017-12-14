40630ce50ca40d15de6db0dc42e73ec8244d7cc1
nixawk <hap.ddup@gmail.com>
2017-12-13 22:21:25 -0500

    add _start

---
+++ src/basic/hello-_start.c
@@ -0,0 +1,54 @@
+// $ gcc hello-_start.c -nostartfiles
+// $ ./a.out
+// custom program entry
+
+// Tested on Ubuntu 17.04
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void program_entry(void);
+
+
+void
+_start(void)
+{ 
+    program_entry();
+}
+
+void
+program_entry(void)
+{
+    printf("custom program entry\n");
+    exit(0);
+}
+
+
+/*
+
+// $ gcc program_entry.c -e __start
+// $ ./a.out
+// custom program entry
+
+// Tested on Mac OSX
+
+
+#include <stdio.h>
+
+void program_entry(void);
+
+
+void
+_start(void)
+{ 
+    program_entry();
+}
+
+
+void
+program_entry(void)
+{
+    printf("custom program entry\n");
+}
+
+*/
