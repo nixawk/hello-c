42cb416a9f704b1a042f588ba296b4dfb8923929
nixawk <hap.ddup@gmail.com>
2017-12-28 22:08:45 +0800

    get information about memory subsystem

---
+++ src/gnu/22.Resource Usage And Limitation/hello_get_phys_pages.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+#include <sys/sysinfo.h>
+#include <stdio.h>
+
+
+// Function: long int get_phys_pages (void)
+
+// The get_phys_pages function returns the total number of pages of physical memory
+// the system has. To get the amount of memory this number has to multiplied by the
+// page size.
+
+
+// Function: long int get_avphys_pages (void)
+
+// The get_avphys_pages funcrtion returns the number of available pages of physical
+// memory the system has. To get the amount of memory this number has to multiplied
+// by the page size.
+
+
+void
+get_phys_pages_usage(void)
+{
+    long int n = get_phys_pages();
+    printf("get_phys_pages() = %ld\n", n);
+}
+
+
+int
+main(void)
+{
+    get_phys_pages_usage();
+    return 0;
+}
---
+++ src/gnu/22.Resource Usage And Limitation/hello_getpagesize.c
@@ -0,0 +1,27 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+
+
+// Function: int getpagesize (void)
+
+// The getpagesize function returns the page size of the process. This value is fixed
+// for the runtime of the process but can vary in different runs of the application.
+
+void
+getpagesize_usage(void)
+{
+    int n = getpagesize();
+    printf("getpagesize() = %d\n", n);
+}
+
+
+int
+main(void)
+{
+	getpagesize_usage();
+	return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_mono/libc.html#toc-Resource-Usage-And-Limitation-1
