628949f2ebf0f4758a208931ee6bef51ab6c0b1b
nixawk <hap.ddup@gmail.com>
2018-01-01 22:00:33 -0500

    add __executable_start

--- src/gnu/26.Processes/hello_etext_edata_end.c
+++ src/gnu/26.Processes/hello_etext_edata_end.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-extern char etext, edata, end;
+
+extern char __executable_start, etext, edata, end;
 
 void
 process_layout(void)
 {
-    printf("etext addr: %p\n", &etext); // addr of the end of the program text
-    printf("edata addr: %p\n", &edata); // addr of the end of the program data
+    printf("executable start: %p\n", &__executable_start);
+    printf("etext addr: %p\n", &etext);
+    printf("edata addr: %p\n", &edata);
     printf("end addr: %p\n", &end);
+
+    printf("text size: %#x\n", (int)(&etext - &__executable_start)); // right ?
+    printf("data size: %#x\n", (int)(&edata - &etext));
+    printf("bss  size: %#x\n", (int)(&end - &edata));
 }
 
 
@@ -21,3 +27,4 @@ main(void)
 
 // https://stackoverflow.com/questions/1765969/where-are-the-symbols-etext-edata-and-end-defined
 // https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/4/html/Using_ld_the_GNU_Linker/scripts.html
+// https://stackoverflow.com/questions/7370407/get-the-start-and-end-address-of-text-section-in-an-executable
