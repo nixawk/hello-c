114e023414ae6b404787fcf3e3449e2cd0ea97c4
nixawk <hap.ddup@gmail.com>
2018-01-01 22:34:41 -0500

    add program_invocation_name / program_invocation_short_name

---
+++ src/gnu/26.Processes/hello_program_invocation_name.c
@@ -0,0 +1,28 @@
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <stdio.h>
+
+
+void
+show_program_invocation_name(void)
+{
+    printf("program_invocation_name: %s\n", program_invocation_name);
+    printf("program_invocation_short_name: %s\n", program_invocation_short_name);
+}
+
+int
+main(void)
+{
+    show_program_invocation_name();
+    return 0;
+}
+
+
+/*
+
+$ /tmp/a.out 
+program_invocation_name: /tmp/a.out
+program_invocation_short_name: a.out
+
+*/
