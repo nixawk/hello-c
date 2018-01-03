4f9466cabccb28841addb5e0dca48f7782b444cb
nixawk <hap.ddup@gmail.com>
2018-01-03 04:16:52 -0500

    fork_stdio_buf

---
+++ src/gnu/26.Processes/hello_fork_stdio_buf.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int
+main(void)
+{
+    setbuf(stdout, NULL);  /* Fix: disable buffer cache */
+
+    printf("Hello, World\n");
+    write(STDOUT_FILENO, "Ciao\n", 5);
+
+    if (fork() == (int) -1)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Both child and parent continue execution here */
+
+    exit(EXIT_SUCCESS);
+}
+
+
+/*
+
+$ ./a.out
+Hello, World
+Ciao
+
+
+$ ./a.out > a
+$ cat a
+Ciao
+Hello, World    --- double printf here
+Hello, World
+
+*/
+
+
+// Fix:
+// setbuf(stdout, NULL);  /* Fix: disable buffer cache */
+
+// http://man7.org/linux/man-pages/man2/fork.2.html
+// http://man7.org/linux/man-pages/man3/setbuf.3.html
