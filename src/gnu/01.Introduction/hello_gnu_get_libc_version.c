fe1391cd6e036489f8f077f1e6f35c03486df748
nixawk <hap.ddup@gmail.com>
2018-01-08 22:32:10 -0500

    add gnu_get_libc_version()

---
+++ src/gnu/01.Introduction/hello_gnu_get_libc_version.c
@@ -0,0 +1,26 @@
+#include <gnu/libc-version.h>
+#include <stdio.h>
+
+
+void
+glibc_ver(void)
+{
+    printf("gnu_get_libc_version() = %s\n", gnu_get_libc_version());
+}
+
+
+int
+main(void)
+{
+    glibc_ver();
+    return 0;
+}
+
+
+/*
+
+$ gcc test.c 
+$ ./a.out 
+gnu_get_libc_version() = 2.24
+
+*/
