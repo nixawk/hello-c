4c875374844c5a6ac8255a0b3c87a1dd0d06d280
nixawk <hap.ddup@gmail.com>
2018-01-09 02:31:12 -0500

    update lseek()

--- src/gnu/libc/13.Low-Level Input and Output/hello_lseek.c
+++ src/gnu/libc/13.Low-Level Input and Output/hello_lseek.c
@@ -1,6 +1,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 // off_t lseek (int filedes, off_t offset, int whence)
@@ -20,32 +22,79 @@
 
 // off64_t lseek64 (int filedes, off64_t offset, int whence)
 
+#ifndef BUF_SIZE
+#define BUF_SIZE 4096
+#endif
+
 
 void
-lseek_usage(void)
+get_openfile_offset(const char *filename)
 {
-    int fd;
-    char data[64] = "";
+    int fd;               /* file descriptor */
+    int rc;               /* count of read */
+    char buf[BUF_SIZE];   /* buffer for read */
+    off_t offset;         /* offset of the open file */
 
-    fd = open("/etc/passwd", O_RDONLY, S_IRWXU);
-    if (fd == -1)
+    /* open file */
+
+    if ((fd = open(filename, O_RDONLY, S_IRUSR | S_IRGRP | S_IROTH)) == -1)
     {
-        fprintf(stderr, "open() failed\n");
-    } else {
-        lseek(fd, -10, SEEK_END);       /* read the last 10 characters */
-        read(fd, data, sizeof(data));
-        printf("read data: %s\n", data);
-        close(fd);
+        perror("open()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* read file, and move file cursor position */
+
+    if ((rc = read(fd, buf, BUF_SIZE)) == -1)
+    {
+        perror("read()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* get file cursor position */
+    if ((offset = lseek(fd, (off_t) 0, SEEK_CUR)) == -1)
+    {
+        perror("lseek()");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("file cursor position: %ld\n", (long) offset);
+
+    /* close file, and avoid memory leak */
+
+    if (close(fd) == -1)
+    {
+        perror("close()");
+        exit(EXIT_FAILURE);
     }
 }
 
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-    lseek_usage();
-    return 0;
+    if ((argc != 2) || (strcmp("--help", argv[1])) == 0)
+    {
+        printf("[*] %s <filename>", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    get_openfile_offset(argv[1]);
+
+    exit(EXIT_SUCCESS);
 }
+
+/*
+
+$ gcc -DBUF_SIZE=2 hello_lseek.c
+$ ./a.out 1.txt
+file cursor position: 2
+
+$ gcc -DBUF_SIZE=3 hello_lseek.c
+$ ./a.out 1.txt
+file cursor position: 3
+
+*/
 
 
 // https://www.gnu.org/software/libc/manual/html_node/File-Position-Primitive.html#File-Position-Primitive
