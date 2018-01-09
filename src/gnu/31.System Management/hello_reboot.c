5b6cb6809bf20af1c9a99af90011dadc2d44662f
nixawk <hap.ddup@gmail.com>
2018-01-09 00:58:59 -0500

    update reboot() desc

--- src/gnu/31.System Management/hello_reboot.c
+++ src/gnu/31.System Management/hello_reboot.c
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 
 
+// LINUX_REBOOT_MAGIC1
+
+// The magic numbers are so that a memory error that flips bits in the system
+// call number which results in the reboot system call's number won't reboot
+// the machine unless the memory error also happens to change the arguments
+// to the magic numbers.
+
+
 void
 reboot_usage(void)  /* reboot computer */
 {
@@ -26,3 +34,6 @@ main(void)
 
 
 // http://man7.org/linux/man-pages/man2/reboot.2.html
+// https://elixir.free-electrons.com/linux/v4.3/source/include/uapi/linux/reboot.h#L8
+// https://unix.stackexchange.com/questions/145502/what-is-the-use-of-magic-arguments-in-linux-reboot-system-call
+// https://superuser.com/questions/640267/what-is-the-purpose-of-the-magic-numbers-in-linux-reboot
