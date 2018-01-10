a2bbadb988d2c04f9d94ceccade6822f7ca8791e
nixawk <hap.ddup@gmail.com>
2018-01-10 01:46:42 -0500

    stdin VS STDIN_FILENO

---
+++ src/gnu/libc/12.Input and Output Streams/hello_stdin_vs_STDIN_FILENO.c
@@ -0,0 +1,23 @@
+// Standard Streams
+
+// FILE * stdin
+// FILE * stdout
+// FILE * stderr
+
+#include <stdio.h>
+
+void
+stdin_vs_STDIN_FILENO(void)
+{
+    printf("STDIN_FILENO, is a file descriptor, a number\n");
+    printf("stdin       , is a file stream, a structure\n");
+}
+
+int
+main(void)
+{
+    stdin_vs_STDIN_FILENO();
+    return 0;
+}
+
+// https://www.gnu.org/software/libc/manual/html_node/Standard-Streams.html#Standard-Streams
