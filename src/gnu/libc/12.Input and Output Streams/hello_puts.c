837c93b654049650ef6485bc2e9e33560a1d88a4
nixawk <hap.ddup@gmail.com>
2018-01-10 18:26:27 -0500

    add puts() and fputs()

---
+++ src/gnu/libc/12.Input and Output Streams/hello_puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Function: int fputs (const char *s, FILE *stream)
+// Function: int puts (const char *s)
+
+void
+fputs_and_puts(void)
+{
+    char s[] = "hello, world\n";
+
+    fputs(s, stdout);
+    puts(s);
+}
+
+int
+main(void)
+{
+    fputs_and_puts();
+    return 0;
+}
+
+// https://www.gnu.org/software/libc/manual/html_node/Simple-Output.html#Simple-Output
