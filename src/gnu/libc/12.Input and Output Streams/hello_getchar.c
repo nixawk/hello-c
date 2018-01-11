80019f5485b6beba9ca46ce6fd7a3389138cb8dd
nixawk <hap.ddup@gmail.com>
2018-01-10 19:17:20 -0500

    add getchar() / fgetc() / getchar()

---
+++ src/gnu/libc/12.Input and Output Streams/hello_getchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void
+fgetc_getc_getchar(void)
+{
+    int c1, c2, c3;
+
+    /* Input a character */
+
+    c1 = fgetc(stdin);
+    c2 = getc(stdin);
+    c3 = getchar();
+
+    /* Output a character */
+
+    putchar(c1);
+    putchar(c2);
+    putchar(c3);
+}
+
+int
+main(void)
+{
+    fgetc_getc_getchar();
+    return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_node/Character-Input.html#Character-Input
