426ddc6ba297fd774402a47191cf7b0cf2e51eca
nixawk <hap.ddup@gmail.com>
2018-01-10 23:35:21 -0500

    add asprintf()

---
+++ src/gnu/libc/12.Input and Output Streams/hello_asprintf.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+
+char *
+make_message(char *name, char *value)
+{
+    char *result;
+    if (asprintf(&result, "value of %s is %s", name, value) < 0)
+        return NULL;
+    return result;
+}
+
+int
+main(void)
+{
+    char *buffer;
+
+    buffer = make_message("name", "smith keen");
+    printf("%s", buffer);
+
+    return 0;
+}
+
+
+// https://www.gnu.org/software/libc/manual/html_mono/libc.html#Formatted-Output-Functions
+// http://man7.org/linux/man-pages/man3/asprintf.3.html
