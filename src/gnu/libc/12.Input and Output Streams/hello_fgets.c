10430e26effc15ed0e0e8cd8a2df818473e53eb4
nixawk <hap.ddup@gmail.com>
2018-01-10 20:35:51 -0500

    add fgets()

---
+++ src/gnu/libc/12.Input and Output Streams/hello_fgets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 4096
+
+// char * fgets (char *s, int count, FILE *stream)
+
+void
+fgets_usage(void)
+{
+    char buf[BUF_SIZE];
+
+    if (fgets(buf, BUF_SIZE, stdin) == NULL)
+    {
+        perror("fgets");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        fputs(buf, stdout);
+    }
+}
+
+int
+main(void)
+{
+    fgets_usage();
+    return 0;
+}
+
+// https://www.gnu.org/software/libc/manual/html_node/Line-Input.html#Line-Input
+// http://man7.org/linux/man-pages/man3/fgets.3.html
