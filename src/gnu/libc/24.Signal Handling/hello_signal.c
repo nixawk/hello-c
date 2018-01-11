a9f6171c6457a17166c2f0dd064b4028415cf2ea
nixawk <hap.ddup@gmail.com>
2018-01-11 03:02:47 -0500

    update signal()

--- src/gnu/libc/24.Signal Handling/hello_signal.c
+++ src/gnu/libc/24.Signal Handling/hello_signal.c
@@ -1,50 +1,44 @@
 #include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// sighandler_t signal (int signum, sighandler_t action)
+// typedef void (*sighandler_t)(int);
+// sighandler_t signal(int signum, sighandler_t handler);
 
-// The [signal] function establishes action as the action for the signal [signum].
-// The first argument, [signum], identifiers the signal whose behavior you want to
-// control, and should be a signal number.
-
-// If you set the action for a signal to [SIG_IGN], or if you set it to [SIG_DFL]
-// and the default action is to ignore that signal, then any pending signals of
-// that type are discarded (even if they are blocked). Discarding the pending 
-// signals means that they will never be delivered, not even if you subsequently 
-// specify another action and unblock this kind of signal.
+static int c;   /* ctrl-c count*/
 
 void
-signal_callback(int signum)
+handler(int sig)
 {
-    printf("PID: %d, SIGNUM: %s\n", getpid(), strsignal(signum));
-}
-
-
-void
-signal_usage(void)
-{
-    if (signal(SIGINT, signal_callback) == SIG_ERR)
+    printf("\tCtrl + C catched\n");
+    c++;
+    if (c > 3)
     {
-        printf("can't catch SIG_INT\n");
-        exit(1);
-    }
-
-    for (;;)
-    {
-        pause();
+        printf("exit the program\n");
+        exit(EXIT_SUCCESS);
     }
 }
-
 
 int
 main(void)
 {
-    signal_usage();
-    return 0;
+    if (signal(SIGINT, handler) == SIG_ERR)
+    {
+        perror("signal");
+        exit(EXIT_FAILURE);
+    }
+
+    for (;;)
+    {
+        printf("waiting for singal...\n");
+        sleep(1);
+    }
+
+    exit(EXIT_SUCCESS);
 }
 
+// http://man7.org/linux/man-pages/man7/signal.7.html
 // https://www.gnu.org/software/libc/manual/html_node/Signal-Handling.html
 // https://www.gnu.org/software/libc/manual/html_node/Basic-Signal-Handling.html#Basic-Signal-Handling
+
