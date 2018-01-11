4e85828550778c83d8ec18775c54fa76ef1291ed
nixawk <hap.ddup@gmail.com>
2018-01-11 04:57:23 -0500

    add sigemptyset()/sigfillset()/sigaddset()/sigdelset()/sigismember()

---
+++ src/gnu/libc/24.Signal Handling/hello_sigemptyset.c
@@ -0,0 +1,72 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void
+signal_set(void)
+{
+    sigset_t sig;
+    int sigret;
+
+    /* sig initializes - sigemptyset / sigfillset */
+
+    if (sigemptyset(&sig) == -1)
+    {
+        perror("sigemptysig");
+        exit(EXIT_FAILURE);
+    }
+
+    /* sig add */
+
+    if (sigaddset(&sig, SIGINT) == -1)
+    {
+        perror("sigaddset");
+        exit(EXIT_FAILURE);
+    }
+
+    sigret = sigismember(&sig, SIGINT);
+    if (sigret == -1)
+    {
+        perror("sigismember");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sigret == 1)
+        printf("SIGINT is not a member of sig\n");
+    else
+        printf("SIGINT is a member of sig\n");
+
+    /* sig del */
+
+    if (sigdelset(&sig, SIGINT) == -1)
+    {
+        perror("sigdelset");
+        exit(EXIT_FAILURE);
+    }
+
+    sigret = sigismember(&sig, SIGINT);
+    if (sigret == -1)
+    {
+        perror("sigismember");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sigret == 1)
+        printf("SIGINT is not a member of sig\n");
+    else
+        printf("SIGINT is a member of sig\n");
+
+}
+
+int
+main(void)
+{
+    signal_set();
+    return 0;
+}
+
+
+// http://man7.org/linux/man-pages/man3/sigemptyset.3p.html
+// http://man7.org/linux/man-pages/man3/sigaddset.3p.html
+// http://man7.org/linux/man-pages/man3/sigdelset.3p.html
+// http://man7.org/linux/man-pages/man3/sigismember.3p.html
