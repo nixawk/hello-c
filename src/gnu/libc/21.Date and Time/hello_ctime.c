f841745099f00ea3b3fd4e01b165445a627eb173
nixawk <hap.ddup@gmail.com>
2018-01-18 22:31:05 -0500

    add ctime()

--- src/gnu/libc/21.Date and Time/hello_ctime.c
+++ src/gnu/libc/21.Date and Time/hello_ctime.c
@@ -1,48 +1,33 @@
 #include <time.h>
 #include <stdio.h>
-
-
-// Function: char * ctime (const time_t *time)
-
-// The ctime function is similar to asctime, except that you specify the
-// calendar time argument as a time_t simple time value rather than in
-// broken-down local time format. It is rquivalent to
-
-    // asctime(localtime(time))
-
-// Calling ctime also sets the current zone as if tzset were called.
-
-
-// Function: char * ctime_r (const time_t *time, char *buffer)
-
-// This function is similar to ctime, but places the result in the string
-// pointed to by buffer. It is equivalent to (written using gcc extensions,
-// see Statement Exprs in Porting and Using gcc):
-
-    // ({ struct tm tm; asctime_r (localtime_r (time, &tm), buf); })
-
-// If no error occurred the function returns a pointer to the string the
-// result was written into, it returns buffer. Otherwise it returns NULL.
-
+#include <stdlib.h>
 
 void
-ctime_usage(void)
+print_current_time(void)
 {
-    const time_t time = 1514366148;
-    char *ct;
+    time_t t;  /* time_t */
+    char * s;  /* string */
 
-    ct = ctime(&time);
-    if (ct != NULL)
+    if ((t = time(NULL)) == (time_t) -1)
     {
-        printf("%s\n", ct);
+        perror("time");
+        exit(EXIT_FAILURE);
     }
+
+    if ((s = ctime(&t)) == NULL)
+    {
+        perror("ctime");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("time() = %ld, \n\t %s\n", t, s);
 }
 
 
 int
 main(void)
 {
-    ctime_usage();
+    print_current_time();
     return 0;
 }
 
