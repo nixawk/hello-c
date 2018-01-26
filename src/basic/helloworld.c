8c54bb999af0a435c5fc19549cb9d752a70b97f9
nixawk <hap.ddup@gmail.com>
2018-01-26 02:36:04 -0500

    update helloworld.c

--- src/basic/helloworld.c
+++ src/basic/helloworld.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
-int main(void)
+int
+main(void)
 {
-  printf("Helloworld");
-  getchar();
-  return 0;
+    /*
+
+        printf never supplies a newline automatically
+
+        \n represents only a single character. An escape sequence
+        like \n provides a general and extension mechanism for
+        represneting hard-to-type or invisible characters.
+
+           \n for newline
+           \t for tab
+           \b for backspace
+           \" for double quote
+           \\ for the backslash itself
+
+    */
+
+    printf("hello, world\n");
+
+    return 0;
 }
+
+
+/*
+
+$ gcc helloc.c
+helloc.c: In function ‘main’:
+helloc.c:7:12: warning: unknown escape sequence: '\c'
+     printf("hello, world\c");
+            ^~~~~~~~~~~~~~~~
+
+*/
