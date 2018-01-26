0c4f4d3bbd4224ab73ff4151d2b189377ddcdacd
nixawk <hap.ddup@gmail.com>
2018-01-26 02:55:08 -0500

    basic data types in c

---
+++ src/basic/hello_basic_data_types.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+    /*
+
+        char   - character - a single byte
+        int    - integer
+        short  - short integer
+        long   - long integer
+        float  - single-precision floating point
+        double - double-precision floating point
+
+        The size of these objects are also machine-dependent.
+        There are also arrays, structures and unions of these basic types,
+        pointers to them, and functions that return them.
+
+    */
+
+    printf("sizeof(char) = %lu\n", sizeof(char));
+    printf("sizeof(int) = %lu\n", sizeof(int));
+    printf("sizeof(short) = %lu\n", sizeof(short));
+    printf("sizeof(long) = %lu\n", sizeof(long));
+    printf("sizeof(float) = %lu\n", sizeof(float));
+    printf("sizeof(double) = %lu\n", sizeof(double));
+
+    return 0;
+}
+
+
+/*
+
+root@lab:/tmp# ./a.out 
+sizeof(char) = 1
+sizeof(int) = 4
+sizeof(short) = 2
+sizeof(long) = 4
+sizeof(float) = 4
+sizeof(double) = 8
+
+root@lab:/tmp# uname -r
+4.6.0-kali1-686-pae
+
+*/
