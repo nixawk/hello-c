d669e3960f10378f56692aa45425acff230bcbe5
nixawk <hap.ddup@gmail.com>
2018-01-26 03:56:23 -0500

    add Symbolic constant

--- src/basic/define-macro-constant.c
+++ src/basic/define-macro-constant.c
@@ -1,14 +1,41 @@
+// Symbolic Constants
+
+/*
+
+A #define line defines a symbolic name or symbolic constant to be a
+particular string of characters;
+
+    #define name replace text
+
+Thereafter, any occurrence of name (not in quotes and not part of another
+name) will be replaced by the corresponding replacement text. The name has
+the same form as a variable name; a sequence of letters and digits that
+begins with a letter. The replacement text can be any sequence of characters;
+it is not limited to numbers.
+
+Symbolic constant names are conventionally written in upper case so they
+can be readily distinguished from lower case variable names.
+
+*/
+
 #include <stdio.h>
 
-#define max 65535
+#define   LOWER  0     /* lower limit of table */
+#define   UPPER  300   /* upper limit */
+#define   STEP   20    /* step size */
 
 int
 main(void)
 {
-    printf("max is %d\n", max);
+    int fahr;
+
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
+    {
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
+
     return 0;
 }
-
 
 /*
 
