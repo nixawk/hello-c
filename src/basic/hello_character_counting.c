85c8e6ef0ad788f7b2530eb3c9abd2f9f016c9b4
nixawk <hap.ddup@gmail.com>
2018-01-26 04:38:27 -0500

    character counting

---
+++ src/basic/hello_character_counting.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int
+main(void)
+{
+    long nc;
+
+    nc = 0;
+    while (getchar() != EOF)
+        ++nc;
+
+    /*
+    ++, which means increment by one. You could instead write nc = nc + 1,
+    but ++nc is more concise and often more efficient.
+
+    What's the difference between ++nc and nc++ ?
+    */
+
+    printf("%ld\n", nc);
+
+    return 0;
+}
+
+
+/*
+$ echo AAAAAAA | ./a.out
+8
+*/
