ce5a4d3e1e982f431d5c9b0fac975ae78e74f69c
nixawk <hap.ddup@gmail.com>
2018-04-29 01:31:31 -0500

    add __find_module

---
+++ src/kernel/api/__find_module.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("LK");
+MODULE_VERSION("1.0.0");
+MODULE_DESCRIPTION("Module for __find_module");
+
+static int __init __find_module_init(void);
+static void __exit __find_module_exit(void);
+
+static int
+__init __find_module_init(void)
+{
+        const char *module_name;
+        struct module *fmodule;
+
+        module_name = "ext4";
+        fmodule = find_module(module_name);
+        if (fmodule != NULL)
+        {
+                printk(KERN_INFO "module->name: %s\n", fmodule->name);
+        }
+        else
+        {
+                printk(KERN_ERR "failed to find module !\n");
+        }
+
+        return 0;
+}
+
+static void
+__exit __find_module_exit(void)
+{
+        printk(KERN_INFO "module exits ok !\n");
+}
+
+module_init(__find_module_init);
+module_exit(__find_module_exit);
+
