b000042c754fd3052cdda10b21000521ebfbd545
nixawk <hap.ddup@gmail.com>
2018-05-02 05:26:31 -0500

    What's the difference between kmalloc and vmalloc ?

---
+++ src/kernel/api/kmalloc.c
@@ -0,0 +1,53 @@
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("LK");
+MODULE_DESCRIPTION("Module for kmalloc");
+
+static int __init kmalloc_init(void);
+static void __exit kmalloc_exit(void);
+void *addr = NULL;
+#define SIZE 4096
+
+static int
+__init kmalloc_init(void)
+{
+        addr = kmalloc(SIZE, GFP_KERNEL);
+        if (!addr)
+        {
+                printk("failed to kmalloc\n");
+                return -ENOMEM;
+        }
+        printk("kmalloc successfully\naddr=0x%p\n", addr);
+
+        return 0;
+}
+
+static void
+__exit kmalloc_exit(void)
+{
+        if (addr)
+        {
+                kfree(addr);
+                printk("kfree successfully\n");
+        }
+        printk("module exits ok !\n");
+}
+
+module_init(kmalloc_init);
+module_exit(kmalloc_exit);
+
+// https://elixir.bootlin.com/linux/latest/source/include/linux/slab.h#L500
+
+/*
+
+# dmesg -k -w
+
+[ 1074.230603] kmalloc successfully
+               addr=0x000000004cfacbdf
+[ 1075.732545] kfree successfully
+[ 1075.732545] module exits ok !
+
+*/
---
+++ src/kernel/api/vmalloc.c
@@ -0,0 +1,55 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/vmalloc.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("LK");
+MODULE_DESCRIPTION("Module for vmalloc");
+
+static int __init my_vmalloc_init(void);
+static void __exit my_vmalloc_exit(void);
+void *addr = NULL;
+
+static int
+__init my_vmalloc_init(void)
+{
+        unsigned long size = 4096;
+
+        addr = vmalloc(size);
+        if (!addr)
+        {
+                printk("failed to vmalloc\n");
+                return -ENOMEM;
+        }
+
+        printk("vmalloc successfully,\naddr=0x%p\n", addr);
+
+        return 0;
+}
+
+static void
+__exit my_vmalloc_exit(void)
+{
+        if (addr)
+        {
+                vfree(addr);
+                printk("vfree successfully\n");
+        }
+        printk("module exits ok !\n");
+}
+
+module_init(my_vmalloc_init);
+module_exit(my_vmalloc_exit);
+
+// https://elixir.bootlin.com/linux/latest/source/include/linux/vmalloc.h#L71
+
+/*
+
+# dmesg -k -w
+
+[ 1721.424860] vmalloc successfully,
+               addr=0x00000000d10d253d
+[ 1725.472210] vfree successfully
+[ 1725.472211] module exits ok !
+
+*/
