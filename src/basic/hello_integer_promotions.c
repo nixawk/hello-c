68984e90dd51aafb49d88d904a2ca92f20d29215
Nixawk <hap.ddup@gmail.com>
2018-06-06 10:13:09 +0800

    update demo for integer promotions

--- src/basic/hello_integer_promotions.c
+++ src/basic/hello_integer_promotions.c
@@ -2,43 +2,39 @@
 
 /*
 
-Some data types like char , short int take less number of bytes than int, these data types are automatically promoted to
-int or unsigned int when an operation is performed on them. This is called integer promotion.
+Some data types like char , short int take less number of bytes than int,
+these data types are automatically promoted to int or unsigned int when
+an operation is performed on them. This is called integer promotion.
 
-For example no arithmetic calculation happens on smaller types like char, short and enum.
-They are first converted to int or unsigned int, and then arithmetic is done on them.
-If an int can represent all values of the original type, the value is converted to an int.
-Otherwise, it is converted to an unsigned int.
+For example no arithmetic calculation happens on smaller types like char,
+short and enum. They are first converted to int or unsigned int, and then
+arithmetic is done on them. 
 
 */
 
-int integer_promotions_char(void)
+void
+integer_promotions(void)
 {
-    char a = 0xfb;
-    unsigned char b = 0xfb;
-  
-    if (a == b)
-      printf("Same\n");
-    else
-      printf("Not Same\n");  // output here.
-    return 0;
-}
+    char c1 = 0xff, c2 = 0xdd, c3 = 0x01;
+    char cX;
 
-int integer_promotions_int(void)
-{
-   unsigned int x = -1;
-   int y = ~0;
+    // The expression (c1 * c2 + c3) seems to cause arithmetic overflow
+    // because signed characters can have values only from -128 to 127
+    // (in most of the C compilers), and the value of subexpression
+    // (c1 * c2 = 0x23) is (0xdc23) which is greater than 128. But integer
+    // promotion happens here in arithmetic done on char types and we get
+    // the appropriate result without any overflow.
 
-   if (x == y)
-      printf("same\n");     // output here.
-   else
-      printf("not same\n");
-   return 0;
+    cX = c1 * c2 + c3;
+
+    printf("char: %x, %d, %c\n", cX, cX, cX);
 }
 
 int  main(void)
 {
-    integer_promotions_char();
-    integer_promotions_int();
+    integer_promotions();
+
     return 0;
 }
+
+// https://www.geeksforgeeks.org/integer-promotions-in-c/
