425b419e5d674e67b04b601a9eb0e3fb937a7ee8
Nixawk <hap.ddup@gmail.com>
2018-06-06 15:49:34 +0800

    bitwise operators

---
+++ src/basic/Operators/hello_bitwise_operators.c
@@ -0,0 +1,40 @@
+// Bitwise operators
+
+#include <stdio.h>
+
+void
+bitwise_operators(void)
+{
+    int i = 1;
+    int j = 2;
+
+    printf("i = %d, j = %d\n", i, j);
+    printf("~i = %d\n", ~i);           /* bitwise NOT */
+    printf("i & j = %d\n", i & j);     /* bitwise AND */
+    printf("i | j = %d\n", i | j);     /* bitwise OR */
+    printf("i ^ j = %d\n", i ^ j);     /* bitwise XOR */
+    printf("i << j = %d\n", i << j);   /* bitwise left shift */
+    printf("i >> j = %d\n", i >> j);   /* bitwise right shift */
+}
+
+int
+main(void)
+{
+    bitwise_operators();
+    return 0;
+}
+
+/*
+
+$ ./a.out
+i = 1, j = 2
+~i = -2
+i & j = 0
+i | j = 3
+i ^ j = 3
+i << j = 4
+i >> j = 0
+
+*/
+
+// https://en.wikipedia.org/wiki/Operators_in_C_and_C%2B%2B#Arithmetic_operators
