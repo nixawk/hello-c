a956ea7ec0d26f29053d7914c33e0d698b8c7673
Nixawk <hap.ddup@gmail.com>
2018-06-10 23:36:00 +0800

    add endswith

---
+++ src/basic/hello_x_endswith.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+
+
+bool
+endswith(s, suffix)
+    char *s;
+    char *suffix;
+{
+    assert(s != NULL && suffix != NULL);
+    return strncmp(
+        s + strlen(s) - strlen(suffix),
+        suffix,
+        strlen(suffix)
+    ) == 0;
+}
+
+int
+main(void)
+{
+    endswith("hello", "lo") ? printf("yes\n") : printf("no\n");
+    // endswith("hello", NULL);
+
+    return 0;
+}
