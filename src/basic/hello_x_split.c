c930987c7ad3a981ad49d804c011a78babac7d89
Nixawk <hap.ddup@gmail.com>
2018-06-11 00:11:13 +0800

    add str split

---
+++ src/basic/hello_x_split.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void
+split(void)
+{
+    char s[] = "aaa.bbb,ccc;ddd:eee!fff-";
+    char sep[] = " .,;:!-";
+
+    char *token;
+
+    for (
+        token = strtok(s, sep);
+        token;
+        token = strtok(NULL, sep))
+    {
+        printf("%s\n", token);
+    }
+
+    if (token)
+        free(token);
+}
+
+int
+main(void)
+{
+    split();
+    return 0;
+}
