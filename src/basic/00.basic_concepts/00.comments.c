dafc1fe1e20802a037e43ddf0ac9b597e87c7eaa
nixawk <hap.ddup@gmail.com>
2018-12-03 21:15:19 +0800

    comments

---
+++ src/basic/00.basic_concepts/00.comments.c
@@ -0,0 +1,21 @@
+/*
+ * C-style comments are usually used to comment large blocks of text or small
+ * fragments of code; however, they can be used to comment single lines.
+ *
+ * C++-style comments are usually used to comment single lines of text or code.
+ * however, they can be placed together to form multi-line comments.
+ *
+ */
+
+/* comment (1) - c-style */
+// comment (2) - c++ style
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("this is a c-style comment.\n");	/* line comment */
+	return 0;
+}
+
+// https://en.cppreference.com/w/c/comment
