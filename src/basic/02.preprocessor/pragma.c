2c03ee8517bcb5aac256a7fe8c4efc8c330c299f
nixawk <hap.ddup@gmail.com>
2018-12-04 23:09:19 +0800

    preprocessor - pragma

---
+++ src/basic/02.preprocessor/pragma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "pragma.h"
+
+int main(void)
+{
+	printf("%s\n", program_name);
+	return 0;
+}
+/* https://en.cppreference.com/w/c/preprocessor/impl */
---
+++ src/basic/02.preprocessor/pragma.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/*
+ * #ifndef _PRAGMA_H_
+ * #define _PRAGMA_H_
+ * #endif
+ */
+
+const char program_name[] = "pragma_demo";
