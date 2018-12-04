a0394ff4a296a53e135fdca357a66b5ecc3c6fd7
nixawk <hap.ddup@gmail.com>
2018-12-04 17:35:17 +0800

    keyword double

---
+++ src/basic/01.keywords/double.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double di = 0.5;
+
+	printf("di = %f\n", di);
+	printf("di = %.3f\n", di);
+	return 0;
+}
+
+/* https://en.cppreference.com/w/c/keyword/double */
