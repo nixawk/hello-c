b4dc3fb28b67af162bda6724d99b0416a4fd0e8e
nixawk <hap.ddup@gmail.com>
2018-12-05 16:51:04 +0800

    comparison

---
+++ src/basic/05.operators/comparison.c
@@ -0,0 +1,22 @@
+void comparison(void)
+{
+	int a, b;
+
+	a = 1;
+	b = 2;
+
+	a == b;
+	a != b;
+	a < b;
+	a > b;
+	a <= b;
+	a >= b;
+}
+
+int main(void)
+{
+	comparison();
+	return 0;
+}
+
+/* https://en.cppreference.com/w/c/language/expressions#Constants_and_literals */
