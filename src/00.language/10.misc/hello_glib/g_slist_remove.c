d9864b76e2b5bde59575231d65d4d4121e6c2380
Nixawk <hap.ddup@gmail.com>
2019-06-08 19:54:18 +0800

    glib - g_slist_remove

---
+++ src/00.language/10.misc/hello_glib/g_slist_remove.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <glib.h>
+
+void
+singly_linked_lists(void)
+{
+        GSList * list = NULL;
+
+        printf("The list is now %d items long\n", g_slist_length(list));
+
+        list = g_slist_append(list, "first");
+        list = g_slist_append(list, "second");
+        printf("The list is now %d items long\n", g_slist_length(list));
+
+        list = g_slist_remove(list, "first");
+        printf("The list is now %d items long\n", g_slist_length(list));
+
+        g_slist_free(list);
+}
+
+int
+main(int argc, char *argv[])
+{
+        singly_linked_lists();
+        return 0;
+}
+
+/* https://developer.ibm.com/tutorials/l-glib/ */
