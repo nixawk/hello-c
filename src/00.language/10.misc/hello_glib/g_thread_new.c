7780c9ad3ddfe75d8b88f881f1ab107e41ac6d50
Nixawk <hap.ddup@gmail.com>
2019-06-09 12:25:46 +0800

    glib - g_thread_new

---
+++ src/00.language/10.misc/hello_glib/g_thread_new.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <glib.h>
+
+typedef struct Thread_Args {
+        gchar *s;
+        int n;
+} Thread_Args_t;
+
+gpointer thread_job(gpointer data)
+{
+        struct Thread_Args *t_args = (struct Thread_Args *)data;
+        gint retval = 1;
+
+        printf("s = %s\n", t_args->s);
+        printf("n = %d\n", t_args->n);
+
+        // g_usleep(2000000);
+
+        g_thread_exit((gpointer)&retval);
+
+        return NULL;
+}
+
+void
+demo_g_thread_new(void)
+{
+        for (gint i = 0; i < 5; i++)
+        {
+                Thread_Args_t *t_args = g_malloc0(sizeof(struct Thread_Args));
+
+                t_args->s = "This is a thread message";
+                t_args->n = i;
+ 
+                GThread *thread = g_thread_new(
+                        "thread",
+                        thread_job,
+                        (gpointer)t_args
+                );
+                g_thread_join(thread);
+        }
+
+}
+
+int
+main(void)
+{
+        demo_g_thread_new();
+        return 0;
+}
+
+/*
+
+$ ./a.out
+s = This is a thread message
+n = 0
+s = This is a thread message
+n = 1
+s = This is a thread message
+n = 2
+s = This is a thread message
+n = 3
+s = This is a thread message
+n = 4
+
+*/
+
+/* https://developer.gnome.org/glib/stable/glib-String-Utility-Functions.html#g-strconcat */
+/* https://developer.gnome.org/glib/2.61/glib-Message-Logging.html */
