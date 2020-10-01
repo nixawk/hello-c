781b1b794d14368368361e1b19e5357d9fdcf025
impulse15 <72219443+impulse15@users.noreply.github.com>
2020-10-01 23:07:48 +0530

    Update else.c

--- src/00.language/01.keywords/else.c
+++ src/00.language/01.keywords/else.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void keyword_else(void)
+void keyword_else(void)      /* positive negative function*/
 {
 	int n;
 
@@ -8,12 +8,14 @@ void keyword_else(void)
 	scanf("%d", &n);
 	if (n >= 0)
 		printf("%d > 0\n", n);
+	        printf("%d is positive/n");
 	else
 		printf("%d < 0\n", n);
+	         printf("%d is negative/n");
 }
 
 int main(void)
 {
-	keyword_else();
+	keyword_else(); /*fuction call*/
 	return 0;
 }
